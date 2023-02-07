660c04a9600ec3d7860905b0afc1544d49405505
Stanislav Kovtun <sstanisslaw@gmail.com>
2023-02-07 16:34:22 +0200

    dynamic inline styles

--- src/components/Tasks/Task/Task.js
+++ src/components/Tasks/Task/Task.js
@@ -3,18 +3,21 @@ import React from "react";
 import "./Task.css";
 
 const Task = (props) => {
-  // const [deleteText, setDeleteText] = useState('');
+    // const [deleteText, setDeleteText] = useState('');
 
-  const deleteHandler = () => {
-    // setDeleteText('(Deleted!)');
-    props.onDelete(props.id);
-  };
+    const deleteHandler = () => {
+        // setDeleteText('(Deleted!)');
+        props.onDelete(props.id);
+    };
 
-  return (
-    <li className="task-item" onClick={deleteHandler}>
-      {props.children}
-    </li>
-  );
+    return (
+        <li className="task-item">
+            <div>
+                {props.children}
+            </div>
+            <div className="del-btn" onClick={deleteHandler}>X</div>
+        </li>
+    );
 };
 
 export default Task;
--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -4,26 +4,42 @@ import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
 
 const TaskInput = (props) => {
-  const [inputText, setinputText] = useState("");
+    const [inputText, setInputText] = useState("");
+    const [isInputValid, setIsInputValid] = useState(true);
 
-  const taskInputChangeHandler = (event) => {
-    setinputText(event.target.value);
-  };
+    const taskInputChangeHandler = (event) => {
+        const value = event.target.value;
+        setInputText(value);
+        if (value.trim()) {
+            setIsInputValid(true);
+        }
+    };
 
-  const formSubmitHandler = (event) => {
-    event.preventDefault();
-    props.onAddTask(inputText);
-  };
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        //inputText.trim() && props.onAddTask(inputText);
+        //console.log('setinputText');
+        if (!inputText.trim()) {
+            setIsInputValid(false);
+            console.log(isInputValid);
+            return;
+        } else {
+            props.onAddTask(inputText);
+            setIsInputValid(true);
+            setInputText('');
+        }
+        console.log(isInputValid);
+    };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
-      </div>
-      <Button type="submit">Добавить Задачу</Button>
-    </form>
-  );
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label style={{ color: isInputValid ? 'black' : 'red' }}>Задачи</label>
+                <input style={{ borderColor: isInputValid ? 'black' : 'red' }} type="text" value={inputText} onChange={taskInputChangeHandler} />
+            </div>
+            <Button type="submit">Добавить Задачу</Button>
+        </form>
+    );
 };
 
 export default TaskInput;
