c4aba12c79328d35a8092e04777443b4b0d4bfe1
Stanislav Kovtun <sstanisslaw@gmail.com>
2023-02-08 11:44:12 +0200

    styled components + media queries

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,7 +1,41 @@
 import React, { useState } from "react";
-
+import styled from 'styled-components';
 import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
+
+const FormControl = styled.div`
+
+margin: 0.5rem 0;
+
+& label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+}
+
+& input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+}
+
+& input:focus {
+    outline: none;
+    background: #c8e1e4;
+    border-color: #00358b;
+}
+
+&.invalid input {
+    border-color: red;
+}
+
+&.invalid label {
+    color: red;
+}
+`;
 
 const TaskInput = (props) => {
     const [inputText, setInputText] = useState("");
@@ -19,22 +53,20 @@ const TaskInput = (props) => {
         event.preventDefault();
         if (!inputText.trim()) {
             setIsInputValid(false);
-            //console.log(isInputValid);
             return;
         } else {
             props.onAddTask(inputText);
             setIsInputValid(true);
             setInputText('');
         }
-        //console.log(isInputValid);
     };
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className={`form-control ${!isInputValid ? 'invalid' : ''}`}>
+            <FormControl className={!isInputValid && 'invalid'}>
                 <label>Tasks</label>
                 <input type="text" value={inputText} onChange={taskInputChangeHandler} />
-            </div>
+            </FormControl>
             <Button type="submit">Add a task</Button>
         </form>
     );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,8 @@
-import style from 'styled-components';
+import styled from 'styled-components';
 
-const Button = style.button`
+const Button = styled.button`
 
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     color: white;
@@ -9,16 +10,20 @@ const Button = style.button`
     border-radius: 4px;
     box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
     cursor: pointer;
+
+    @media (min-width: 700px) {
+        width: auto;
+    }
   
-  &:focus {
-    outline: none;
-  }
+    &:focus {
+        outline: none;
+    }
   
-  &:hover,
-  &:active {
-    background: #245fbd;
-    box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
-  }
+    &:hover,
+    &:active {
+        background: #245fbd;
+        box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
+    }
 `;
 
 export default Button;
